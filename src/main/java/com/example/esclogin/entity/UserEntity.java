1e733a7321060bbd8b706deaf250d33158759773
jihhyeong <jinhinjala26@naver.com>
2024-09-27 18:46:34 +0900

    success jwt login and join

---
+++ src/main/java/com/example/esclogin/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package com.example.esclogin.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+    private String role;
+}
